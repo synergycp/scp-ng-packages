ae0908ef04873b89c3b8bf8d46798f33d98325fa
Zane Hooper <zanehoop@gmail.com>
2016-09-01 21:34:52 -0400

    Packages Initial Commit

---
+++ src/packages.routes.js
@@ -0,0 +1,45 @@
+(function () {
+  angular
+    .module('scp.packages')
+    .config(routeConfig);
+
+  /**
+   * @ngInject
+   */
+  function routeConfig($futureStateProvider, $stateProvider, RouteHelpersProvider, PackageLoaderProvider) {
+    var helper = RouteHelpersProvider;
+    $stateProvider.state('app.pkg', {
+      url: '/pkg',
+      abstract: true,
+      template: helper.dummyTemplate,
+    });
+    $futureStateProvider.addResolve(function (PackageLoader) {
+      "ngInject";
+
+      return PackageLoader;
+    });
+
+    function deprecated() {
+      $futureStateProvider
+        .futureState({
+          stateName: 'app.pkg',
+          url: '/pkg',
+          type: 'package',
+        });
+
+      $futureStateProvider.stateFactory('package', packageStateFactory);
+    }
+
+    /**
+     * @ngInject
+     */
+    function packageStateFactory(futureState, PackageLoader) {
+
+
+      return PackageLoader.then(function() {
+        // If we return the files object here,
+        // $futureStateProvider chokes.
+      });
+    }
+  }
+})();
